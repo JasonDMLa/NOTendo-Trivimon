7d118f6e1891df7239262f83ea2d93f0707e00a6
james <james_lau2001@outlook.com>
2024-10-03 09:37:11 +0100

    fixed the punctuation in the api calling

--- src/utils/questionData.js
+++ src/utils/questionData.js
@@ -1,12 +1,31 @@
 import { getData } from "../data/apiCalls";
 
 export const questionData = async (amount, category, difficulty) => {
- 
-    const results = await getData(amount, category, difficulty);
-    const questions = results.map((currentQuestion) => ({
-      question: currentQuestion.question,
-      correctAnswer: currentQuestion.correct_answer,
-      incorrectAnswers: currentQuestion.incorrect_answers,
-    }));
-    return questions;
-  };
+  const results = await getData(amount, category, difficulty);
+  const questions = results.map((currentQuestion) => ({
+    question: currentQuestion.question
+      .replace(/&quot;/g, '"')
+      .replace(/&#039;/g, "'")
+      .replace(/&reg/g, "©")
+      .replace(/&amp;/g, "&")
+      .replace(/&ouml;/g, "ö")
+      .replace(/&deg;/g, "°"),
+    correctAnswer: currentQuestion.correct_answer
+      .replace(/&quot;/g, '"')
+      .replace(/&#039;/g, "'")
+      .replace(/&reg/g, "©")
+      .replace(/&amp;/g, "&")
+      .replace(/&ouml;/g, "ö")
+      .replace(/&deg;/g, "°"),
+    incorrectAnswers: currentQuestion.incorrect_answers.map((answer) =>
+      answer
+        .replace(/&quot;/g, '"')
+        .replace(/&#039;/g, "'")
+        .replace(/&reg/g, "©")
+        .replace(/&amp;/g, "&")
+        .replace(/&ouml;/g, "ö")
+        .replace(/&deg;/g, "°")
+    ),
+  }));
+  return questions;
+};
