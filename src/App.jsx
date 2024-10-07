ae81b4b2c19569e7c4e31aff41c1b0e867fa0112
JasonDMLa <Jason.DM.La@outlook.com>
2024-10-07 08:46:34 +0100

    added a Create account and Login Account feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
 import "./App.css";
 import PhaserGame from "./components/PhaserGame";
-import StartScreen from "./Scenes/StartScreen";
-import ThirdScreen from "./Scenes/VideoGameScene";
-import AccountLogin from "./components/AccountLogin";
-import Login from "./components/Login";
+import LoginAccount from "./components/LoginAccount";
+import { Create } from "phaser";
+import CreateAccount from "./components/CreateAccount";
 
 function App() {
   const [isGameStarted, setIsGameStarted] = useState(false); // State to track if the game has started
 
+  const handleLoginSuccess = () => {
+    setIsGameStarted(true); // Change the state to start the game
+  };
+
   return (
     <div className="App">
-      <Login/>
-      {/* <AccountLogin/> */}
-      {/* {!isGameStarted ? (
-        <StartScreen onStart={() => setIsGameStarted(true)} />
+      {/* <CreateAccount/> */}
+      {!isGameStarted ? (
+        <LoginAccount onStart={handleLoginSuccess} />
       ) : (
         <PhaserGame />
-      )} */}
-      {/* <PhaserGame /> */}
-      {/* <ThirdScreen/> */}
+      )}
     </div>
   );
 }
